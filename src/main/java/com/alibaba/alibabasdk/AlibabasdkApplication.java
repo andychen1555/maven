d09c5b4cc213c043c8aca283e03977ee8edf1db2
andychen <andychen1555@163.com>
2020-09-04 14:48:52 +0800

    init

---
+++ src/main/java/com/alibaba/alibabasdk/AlibabasdkApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.alibabasdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabasdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibabasdkApplication.class, args);
+	}
+
+}
